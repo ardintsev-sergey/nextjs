94a62426f25b8e438fcd34d502a50aa8d468d5ef
Ardintsev Sergey <archunter1369@gmail.com>
2022-10-16 13:40:29 +0300

    Husky

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document {
         return (
             <Html lang="ru">
                 <Head>                    
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />                    
                     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
